50d4bdb9692af2853392166ff2602c9a0488b06f
최민재 <cmjcmj1230@gmail.com>
2023-04-12 20:59:54 +0900

    add first test

--- slog-react/src/App.js
+++ slog-react/src/App.js
@@ -13,8 +13,36 @@ function App() {
 
   axios.get("")
   return (
-    <div className="container" onClick={()=>{sssss({id:12, name="dfasdf"})}}>
-      응애
+    <div className="container" onClick={()=>{sssss({id:12})}}>
+      
+      <body>
+        <table>
+          <thead>
+            <tr>
+              <th>나이</th>
+              <th>이름</th>
+              <th>번호</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>20</td>
+              <td>홍길동</td> 
+              <td>20220001</td>
+            </tr>
+            <tr>
+              <td>21</td>
+              <td>김철수</td>
+              <td>20220002</td>
+            </tr>
+            <tr>
+              <td>19</td>
+              <td>이영희</td>
+              <td>20220003</td>
+            </tr>
+          </tbody>
+        </table>
+      </body>
     </div>
   );
 }
--- slog/app.js
+++ slog/app.js
@@ -1,32 +1,69 @@
 const express = require("express");
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const pool = mysql.createPool({
-  host: "localhost",
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "172.18.0.3",
+  //host: "mysql",
   user: "root",
-  password: "cmj655655@",
+  password: "password",
   database: "slog",
   port:`3306`,
 });
 
-const app = express();
+app.use(express.json());
+app.use(cors());
+
+//json 형태로 데이터 주고받도록 셋팅
+//app.use(bodyParser.json())
+
+connection.connect();
 
 app.get("/students", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-
-    connection.query("SELECT * from students", (error, rows, fields) => {
-      if (error) throw error;
-      console.log("User info is : ", rows);
-      res.send(rows);
-    });
-
-    connection.release();
+  connection.query("SELECT * from students", (error, rows, fields) => {
+    if (error) throw error;
+    console.log("User info is : ", rows);
+    res.send(rows);
   });
 });
+
+app.post("/students", (req, res) => {
+  
+  let values = [req.body.name, req.body.age]
+  let sql = `INSERT INTO students(name, age) VALUES(?, ?)`
+
+  connection.query(sql, values, (error, rows, fields) => {
+    if(error) throw error;
+    res.status(200).json({ status: 200, msg: "학생 추가 성공" });
+  })
+})
+
+app.put("/students/modify", (req, res) => {
+  let values = [req.body.name, req.body.age, req.body.id]
+  let updatesql = "UPDATE students SET name = ?, age = ? WHERE id = ?"
+
+  connection.query(updatesql, values, (error, rows, fields) => {
+      if(error) throw error;
+      res.status(200).json({ status: 200, msg: "수정 성공" });
+  })
+})
+
+app.delete("/students/delete", (req, res) => {
+    let values = [req.body.id]
+    let deletesql = "Delete from students where id = ?"
+
+    connection.query(deletesql, values, (error, rows, fields) => {
+        if(error) throw error;
+        res.status(200).json({ status: 200, msg: "삭제 성공" });
+    })
+})
+
 app.get("/", (req, res) => {
   res.send("hi");
 });
-app.listen("5001", () => {
-  console.log("5001listening");
+app.listen('5000', () => {
+  console.log(app.get('port') + "listening");
 });
